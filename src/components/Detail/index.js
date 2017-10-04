6ddca7daa7e222483fad26c9e9e2edcefa545976
Dennis Alves <dennis.alves@live.com>
2017-10-03 22:49:13 -0300

    Some thin adjustments in shot listing view and some structural changes on shot detail.

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Detail = ({shot}) => {
   if (shot) {
     return (
       <div className="shot-detail-container">
         <div className="shot-detail">
-          <span>{shot.title}</span>
-          <span>{shot.user.name} - ({shot.user.username})</span>
-          <img src={shot.images.hidpi} />
-          <span className="views"></span>
-          <span className="likes"></span>
-          <p dangerouslySetInnerHTML={{ __html: shot.description}}></p>
+
+          <div className="header modal">
+            <Link to="/"><span className="close-button"></span></Link>
+          </div>
+
+          <div className="shot-header">
+            <span>{shot.title}</span>
+            <span>{shot.user.name} - ({shot.user.username})</span>
+          </div>
+
+          <div className="shot-image">
+            <img src={shot.images.hidpi} alt={shot.title} />
+            <div className="shot-numbers">
+              <span className="views"></span>
+              <span className="likes"></span>
+            </div>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: shot.description}}></div>
         </div>
       </div>
     )
